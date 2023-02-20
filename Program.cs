416661de44ece7716df7b1dedefc9311d7599381
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-19 20:43:18 -0800

    ADD: Employee Attendence Program in added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeStatus;
+
+namespace EmployeeAttendance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Attendance.Employee();
+        }
+    }
+}
